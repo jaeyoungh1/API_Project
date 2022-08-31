2487d7d43c4e40810c8144960dfbaddff85a37f0
Jae Hwang <jaeyoung1997@gmail.com>
2022-08-31 15:04:50 -0700

    implemented validation error message to match api documentation

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -6,17 +6,20 @@ const handleValidationErrors = (req, _res, next) => {
     const validationErrors = validationResult(req);
 
     if (!validationErrors.isEmpty()) {
+        let result = {}
         const errors = validationErrors
             .array()
-            .map((error) => `${error.msg}`);
+            .forEach(error => {
+                result[error.param] = error.msg
+            })
 
-        const err = Error('Bad request.');
-        err.errors = errors;
-        err.status = 400;
-        err.title = 'Bad request.';
-        next(err);
-    }
-    next();
+    const err = Error('Validation Error');
+    err.errors = result;
+    err.status = 400;
+    err.title = 'Bad request.';
+    next(err);
+}
+next();
 };
 
 module.exports = {
