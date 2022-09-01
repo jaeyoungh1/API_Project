0e599551229f21b32e6c1932e13cc701db4cd477
Jae Hwang <jaeyoung1997@gmail.com>
2022-09-01 15:56:10 -0700

    ordering the get all spots

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -128,16 +128,16 @@ router.get('/', validatePagination, async (req, res, next) => {
     const spots = await Spot.findAll({
         // required: true,
         // duplicating: false,
-        order: ['id'],
+        order: [['id']],
         include: [
             {
                 model: Review,
                 attributes: [],
             },
-            // {
-            //     model: SpotImage,
-            //     attributes: []
-            // },
+            {
+                model: SpotImage,
+                attributes: []
+            },
         ],
         ...pagination,
         where,
@@ -149,47 +149,48 @@ router.get('/', validatePagination, async (req, res, next) => {
                     sequelize.fn('ROUND', sequelize.fn("AVG", sequelize.col("Reviews.stars")), 2),
                     "avgRating"
                 ],
-                // [
-                //     sequelize.where(
-                //         sequelize.col("SpotImages.preview"), sequelize.col("SpotImages.url"), true),
-                //     "previewImage"
-                // ]
+                [
+                    
+                    sequelize.col("SpotImages.url"),
+                    "previewImage"
+                ]
             ],
         },
-        group: ["Spot.id"],
-        // group: ["Spot.id", "SpotImages.url"],
+        // group: ["Spot.id"],
+        group: ["Spot.id", "SpotImages.url"],
         raw: true
     });
 
-    let Spots = []
+    // let Spots = []
 
-    for (let i = 0; i < spots.length; i++) {
-        let previewImage = await SpotImage.findAll({
-            where: {
-                spotId: spots[i].id,
-                preview: true
-            },
-            attributes: ['url']
-        })
-        let url;
-        let previewImgObj = await previewImage[0]
+    // for (let i = 0; i < spots.length; i++) {
+    //     let previewImage = await SpotImage.findAll({
+    //         where: {
+    //             spotId: spots[i].id,
+    //             preview: true
+    //         },
+    //         attributes: ['url']
+    //     })
+    //     let url;
+    //     let previewImgObj = await previewImage[0]
     
-        if (previewImgObj) {
-            let imgobj = await previewImgObj.toJSON()
-            url = imgobj.url
-        } else url = null
-        console.log(url)
-        let spotObj = await spots[i]
-        spotObj.previewImage = url
-        Spots.push(spotObj)
-    }
+    //     if (previewImgObj) {
+    //         let imgobj = await previewImgObj.toJSON()
+    //         url = imgobj.url
+    //     } else url = null
+    //     console.log(url)
+    //     let spotObj = await spots[i]
+    //     spotObj.previewImage = url
+    //     Spots.push(spotObj)
+    // }
     
-    let result = {}
-    result.Spots = Spots;
-    result.page = page;
-    result.size = size;
+    // let result = {}
+    // result.Spots = Spots;
+    // result.page = page;
+    // result.size = size;
 
-    return res.json(result)
+    // return res.json(result)
+    return res.json({Spots: spots, page, size})
 })
 
 //get all spots from current user
