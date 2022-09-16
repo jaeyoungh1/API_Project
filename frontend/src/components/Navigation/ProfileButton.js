5288fc40b3e5dfed0e51dacc480e9218e35b4ea9
Jae Hwang <jaeyoung1997@gmail.com>
2022-09-16 00:29:52 -0700

    added navigation css

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,7 +1,51 @@
-export const ProfileButton = () => {
+import { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { logout } from '../../store/session'
+
+export const ProfileButton = ({ user }) => {
+    const dispatch = useDispatch()
+    let [showMenu, setShowMenu] = useState(false)
+
+    const openMenu = () => {
+        if (showMenu) return;
+        else setShowMenu(true);
+    }
+
+    useEffect(() => {
+        if (!showMenu) return;
+
+        const closeMenu = () => {
+            setShowMenu(false);
+        };
+
+        document.addEventListener('click', closeMenu);
+
+        return () => document.removeEventListener("click", closeMenu);
+    }, [showMenu]);
+
+    const logoutUser = e => {
+        e.preventDefault()
+        dispatch(logout())
+    }
+
     return (
-        <button className='profile button'>
-            <i class="fa-regular fa-user"></i>
-        </button>
+        <>
+            <div className='dropdownwrapper'>
+                <div className='profilebuttonwrapper'>
+                    <button onClick={openMenu} className='profilebutton'>
+                        <i class="fa-regular fa-user"></i>
+                    </button>
+                </div>
+                {showMenu && (
+                    <ul className='profile dropdown'>
+                        <li className='profileusername' key='username'>{user.username}</li>
+                        <li className='profileemail' key='email'>{user.email}</li>
+                        <li>
+                            <button onClick={logoutUser} className='logout'>Log Out</button>
+                        </li>
+                    </ul>
+                )}
+            </div>
+        </>
     )
 }
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -2,28 +2,34 @@ import { NavLink } from "react-router-dom"
 import { useSelector } from "react-redux"
 import { ProfileButton } from "./ProfileButton"
 
+import './Navigation.css'
 
 export const Navigation = () => {
 
     const currentUser = useSelector(state => state.session.user)
 
+    let sessionLinks;
+    if (currentUser) {
+        sessionLinks = (
+            <ProfileButton user={currentUser} />
+        );
+    } else {
+        sessionLinks = (
+            <>
+                <span><NavLink style={{'text-decoration':'none', 'color': '#45454599'}}to="/login">Log In</NavLink></span>
+                <span><NavLink style={{'text-decoration':'none', 'color': '#45454599'}}to="/signup">Sign Up</NavLink></span>
+            </>
+        );
+    }
+
     return (
-        <nav>
-            {currentUser && (
-                <>
-                    <ul>
-                        <li key='home'><NavLink exact to='/'>Home</NavLink></li>
-                    </ul>
-                    <ProfileButton />
-                </>
-            )}
-            {!currentUser && (
-                <ul>
-                    <li key='home'><NavLink exact to='/'>Home</NavLink></li>
-                    <li key='signup'><NavLink to='/signup'>Sign Up</NavLink></li>
-                    <li key='login'><NavLink to='/login'>Login</NavLink></li>
-                </ul>
-            )}
-        </nav>
-    )
+        <ul>
+            <li className='session'>
+                <NavLink style={{'text-decoration':'none', 'color': '#45454599'}}exact to="/">Home</NavLink>
+                <div className='sessionlinks'>
+                {sessionLinks}
+                </div>
+            </li>
+        </ul>
+    );
 }
