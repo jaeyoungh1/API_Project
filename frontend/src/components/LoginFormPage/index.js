58fddb669a715fee339ec2a8f90762258e38f207
Jae Hwang <jaeyoung1997@gmail.com>
2022-09-15 18:34:29 -0700

    login page routes and css created

---
+++ frontend/src/components/LoginFormPage/index.js
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+import { Redirect } from 'react-router-dom'
+
+import './LoginForm.css';
+
+import { useDispatch, useSelector } from 'react-redux'
+import { login } from '../../store/session'
+
+const LoginFormPage = () => {
+    const dispatch = useDispatch()
+    const [credential, setCredential] = useState('')
+    const [password, setPassword] = useState('')
+    const [errors, setErrors] = useState('')
+
+    let currentUser = useSelector(state => state.session.user)
+    if (currentUser) {
+        return <Redirect to='/' />
+    }
+
+    const onSubmit = async e => {
+        e.preventDefault()
+        setErrors('')
+        const user = {
+            credential,
+            password
+        }
+        return dispatch(login(user))
+            .catch(async (res) => {
+                const data = await res.json();
+                console.log('data:', data)
+                if (data && data.statusCode === 401) setErrors(data.message)
+            })
+    }
+
+    return (
+        <form onSubmit={onSubmit}>
+            {/* <div className='wrapper'> */}
+                <ul>
+                    {errors && <li key={errors}>{errors}</li>}
+                </ul>
+                <div className='login credential'>
+                    <label htmlFor='login credential'>Username or Email</label>
+                    <input id='login credential' type='text'
+                        value={credential}
+                        onChange={e => setCredential(e.target.value)}
+                        required />
+                </div>
+                <div className='login password'>
+                    <label htmlFor='login password'>Password</label>
+                    <input id='login password' type='text'
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                        required />
+                </div>
+                <button type='submit'>Login</button>
+            {/* </div> */}
+        </form>
+    )
+}
+
+export { LoginFormPage }
