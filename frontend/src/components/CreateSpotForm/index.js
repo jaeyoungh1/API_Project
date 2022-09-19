2b3557af8007f8bf308a45de08fa5e481d4cbf46
Jae Hwang <jaeyoung1997@gmail.com>
2022-09-19 13:54:52 -0700

    added country drop down functionality

--- frontend/src/components/CreateSpotForm/index.js
+++ frontend/src/components/CreateSpotForm/index.js
@@ -3,6 +3,8 @@ import { useHistory } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { createOneSpot } from '../../store/spots'
 
+import './spotForm.css'
+
 export const CreateASpot = () => {
     const dispatch = useDispatch()
     const history = useHistory()
@@ -32,11 +34,10 @@ export const CreateASpot = () => {
             description,
             price
             //add preview image, 2 fetches and a thunk
-            //input type password
-            // css text-overflow ...
-            // css object-fit: cover;
-            // mdn input fields 
+
         }
+
+        console.log(submission)
 
         let createdSpot;
         try {
@@ -57,66 +58,116 @@ export const CreateASpot = () => {
     }
     return (
         <>
-        <div id='create-spot-name'>
-            Host Your Spot
-        </div>
-        <div id='create-spot-wrapper'>
-            <div className='errors'>
-                {errors.length > 0 && (
-                    <ul key={errors}>
-                        {errors.map(error => (
-                            <li key={error}>{error}</li>
-                        ))}
-
-                    </ul>
-                )}
+            <div id='create-spot-name'>
+                Host Your Spot
             </div>
-            <form onSubmit={onSubmit}>
-                <input
-                    type='text'
-                    placeholder='name'
-                    value={name}
-                    onChange={e => setName(e.target.value)}>
-                </input>
-                <input
-                    type='text'
-                    placeholder='address'
-                    value={address}
-                    onChange={e => setAddress(e.target.value)}>
-                </input>
-                <input
-                    type='text'
-                    placeholder='city'
-                    value={city}
-                    onChange={e => setCity(e.target.value)}>
-                </input>
-                <input
-                    type='text'
-                    placeholder='state'
-                    value={state}
-                    onChange={e => setState(e.target.value)}>
-                </input>
-                <input
-                    type='text'
-                    placeholder='country'
-                    value={country}
-                    onChange={e => setCountry(e.target.value)}>
-                </input>
-                <input
-                    type='text'
-                    placeholder='description'
-                    value={description}
-                    onChange={e => setDescription(e.target.value)}>
-                </input>
-                <input
-                    type='text'
-                    placeholder='price'
-                    value={price}
-                    onChange={e => setPrice(e.target.value)}>
-                </input>
-                <button id='create-spot-button' type='submit'>Create New Spot</button>
-            </form>
-        </div>
+            <div id='create-spot-wrapper'>
+                <div className='errors-wrapper'>
+                    {errors.length > 0 && (
+                        <ul className='create-spot-errorlist' key={errors}>
+                            <div>Please address the following errors:</div>
+                            {errors.map(error => (
+                                <li className='create-spot-error' key={error}>{error}</li>
+                            ))}
+
+                        </ul>
+                    )}
+                </div>
+                <form className='create-spot-form' onSubmit={onSubmit}>
+
+                    <label className='create-spot-input-title'>Address</label>
+                    <div className='create-spot-input address'>
+                        <input
+                            id='address'
+                            type='text'
+                            placeholder='123 App Academy St'
+                            value={address}
+                            onChange={e => setAddress(e.target.value)}>
+                        </input>
+                    </div>
+                    <label className='create-spot-input-title'>City</label>
+
+                    <div className='create-spot-input='>
+
+                        <input
+                            type='text'
+                            placeholder='San Francisco'
+                            value={city}
+                            onChange={e => setCity(e.target.value)}>
+                        </input>
+                    </div>
+                    <label className='create-spot-input-title'>State/Province</label>
+
+                    <div className='create-spot-input'>
+
+                        <input
+                            type='text'
+                            placeholder='California'
+                            value={state}
+                            onChange={e => setState(e.target.value)}>
+                        </input>
+                    </div>
+                    <label className='create-spot-input-title'>Country</label>
+
+                    <div className='create-spot-input'>
+                        <select id="country" name="country"
+                            value={country}
+                            onChange={e => setCountry(e.target.value)}>
+                            <option value="United States of America">United States of America</option>
+                            <option value="Canada">Canada</option>
+                            <option value="Cayman Islands">Cayman Islands</option>
+                            <option value="Fiji">Fiji</option>
+                            <option value="France">France</option>
+                            <option value="Greece">Greece</option>
+                            <option value="Guam">Guam</option>
+                            <option value="Korea, Republic of">Korea, Republic of</option>
+                            <option value="Macao">Macao</option>
+                            <option value="Maldives">Maldives</option>
+                            <option value="Monaco">Monaco</option>
+                            <option value="Morocco">Morocco</option>
+                            <option value="Switzerland">Switzerland</option>
+                            <option value="Thailand">Thailand</option>                            <option value="United Kingdom">United Kingdom</option>
+                        </select>
+
+                    </div>
+                    <label className='create-spot-input-title'>Name of Your Spot</label>
+
+                    <div className='create-spot-input'>
+                        <input
+                            type='text'
+                            placeholder='App Academy'
+                            value={name}
+                            onChange={e => setName(e.target.value)}>
+                        </input>
+                    </div>
+                    <label className='create-spot-input-title'>Description of Your Spot</label>
+
+                    <div className='create-spot-input'>
+                        <input
+                            type='text'
+                            placeholder='A place where software engineers are devloped!'
+                            value={description}
+                            onChange={e => setDescription(e.target.value)}>
+                        </input>
+                    </div>
+                    <label className='create-spot-input-title'>Price per Night (American Dollars $)</label>
+
+                    <div className='create-spot-input'>
+
+
+                        <input
+                            type='number'
+                            placeholder='123'
+                            min='1'
+                            value={price}
+                            onChange={e => setPrice(e.target.value)}>
+                        </input>
+                    </div>
+                    <div id='create-spot-button-wrapper' n>
+                        <button id='create-spot-button' type='submit'>Create New Spot</button>
+                    </div>
+                </form>
+            </div>
         </>
     )
 }
