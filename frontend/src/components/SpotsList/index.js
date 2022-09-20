fe526f7d09dd31351164704d1e9b20244ba85eb0
Jae Hwang <jaeyoung1997@gmail.com>
2022-09-20 09:48:35 -0700

    added navlinks on home page to single spots

--- frontend/src/components/SpotsList/index.js
+++ frontend/src/components/SpotsList/index.js
@@ -1,4 +1,5 @@
 import { useEffect } from 'react'
+import { NavLink } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { getAllSpots } from '../../store/spots'
 import './SpotsList.css'
@@ -13,20 +14,23 @@ export const SpotsList = () => {
     }, [])
 
 
+
     const spotsInfo = spots.map(spot => {
         return (
-            <div className={`spot ${spot.id}`}>
-                <div className='spot-img-div'><img className='spot-img' alt={spot.previewImage} src={spot.previewImage} /></div>
-                <div className='spot-location-div'>{`${spot.city}, ${spot.state}`}<span style={{ fontWeight: '300' }} id={`rating-${spot.id}`}>{spot.avgRating !== null ? `★ ${spot.avgRating}` : `★ New`}</span></div>
-                <div className='spot-price-div' style={{fontWeight:500}}>{`$${spot.price}`} <span style={{fontWeight:300}}>night</span></div>
-            </div>
+            <NavLink style={{textDecoration: 'none', color:'black'}} key={spot.id} to={`/spots/${spot.id}`}>
+                <div className={`spot ${spot.id}`}  >
+                    <div className='spot-img-div'><img className='spot-img' alt={spot.previewImage} src={spot.previewImage} /></div>
+                    <div className='spot-location-div'>{`${spot.city}, ${spot.state}`}<span style={{ fontWeight: '300' }} id={`rating-${spot.id}`}>{spot.avgRating !== null ? `★ ${spot.avgRating}` : `★ New`}</span></div>
+                    <div className='spot-price-div' style={{ fontWeight: 500 }}>{`$${spot.price}`} <span style={{ fontWeight: 300 }}>night</span></div>
+                </div>
+            </NavLink>
         )
     })
 
     if (!spots) return null
     return (
         <div id='spots-wrapper'>
-                {spotsInfo}
+            {spotsInfo}
         </div>
     )
 }
