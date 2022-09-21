d7c38ae8fe149beb0f05656d0db2f8e01e5d7763
Jae Hwang <jaeyoung1997@gmail.com>
2022-09-21 15:21:52 -0700

    trying to figure out create review

---
+++ frontend/src/components/CreateReviewForm/index.js
@@ -0,0 +1,112 @@
+import { useEffect, useState } from 'react'
+import { useHistory, useParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { createOneReview } from '../../store/reviews'
+
+import '../EditReview/EditReview.css'
+
+export const CreateAReview = () => {
+    const dispatch = useDispatch()
+    const history = useHistory()
+    const { spotId } = useParams()
+
+    const currSpotData = useSelector(state => state.spots.singleSpot.spotData)
+
+
+    const [review, setReview] = useState('')
+    const [stars, setStars] = useState(0)
+    const [url, setUrl] = useState('')
+    const [errors, setErrors] = useState([])
+
+
+    const onSubmit = async e => {
+        e.preventDefault()
+
+        const submission = {
+            review,
+            stars
+        }
+
+        console.log('submission', submission)
+
+        let createdReview;
+        console.log('spotId', typeof spotId)
+        try {
+            createdReview = await dispatch(createOneReview(spotId, submission))
+            console.log('createdReview', createdReview)
+        } catch (err) {
+            if (err) {
+                let errMsgs = err.errors
+                let errMsgsArr = Object.values(errMsgs)
+                setErrors(errMsgsArr)
+            }
+        }
+
+        if (createdReview) {
+            setReview('')
+            setStars(0)
+            setUrl([])
+
+            console.log('REVIEW BEING EDITED', createdReview)
+            setErrors([])
+            history.push(`/my-reviews`)
+        }
+    }
+
+    return (
+        <>
+            <div id='create-review-name'>
+                <h2>Create Review for <span className='review-spot-name'> {currSpotData.name}</span></h2>
+            </div>
+            <div id='create-review-wrapper'>
+                <div className='errors-wrapper'>
+                    {errors.length > 0 && (
+                        <ul className='create-review-errorlist' key={errors}>
+                            <div>Please address the following errors:</div>
+                            {errors.map(error => (
+                                <li className='create-review-error' key={error}>{error}</li>
+                            ))}
+
+                        </ul>
+                    )}
+                </div>
+                <form className='create-review-form' onSubmit={onSubmit}>
+
+                    <label className='create-review-input-title'>Review</label>
+                    <div className='create-review-input-review'>
+                        <textarea
+                            id='review'
+                            placeholder='What a great location..'
+                            value={review}
+                            onChange={e => setReview(e.target.value)}>
+                        </textarea>
+                    </div>
+                    <label className='create-review-input-title'>Rating</label>
+
+                    <div className='create-review-input-rating'>
+
+                        ★<input
+                            type='number'
+                            placeholder='4'
+                            value={stars}
+                            onChange={e => setStars(e.target.value)}>
+                        </input>/5
+                    </div>
+
+                    <label className='create-review-input-title'>Add a Picture</label>
+                    <div className='create-review-input'>
+                        <input
+                            type='text'
+                            placeholder='https://...'
+                            value={url}
+                            onChange={e => setUrl(e.target.value)}>
+                        </input>
+                    </div>
+                    <div id='create-review-button-wrapper' n>
+                        <button id='create-review-button' type='submit'>Create Review</button>
+                    </div>
+                </form>
+            </div>
+        </>
+    )
+}
