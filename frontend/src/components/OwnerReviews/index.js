405816ffb801480f033b92cb7bba747855fd8712
Jae Hwang <jaeyoung1997@gmail.com>
2022-11-11 14:40:55 -0800

    fixed listing button

--- frontend/src/components/OwnerReviews/index.js
+++ frontend/src/components/OwnerReviews/index.js
@@ -1,16 +1,21 @@
 import { useEffect, useState } from "react"
 import { NavLink, Redirect } from "react-router-dom"
 import { useSelector, useDispatch } from "react-redux"
-import { getOwnerReviews } from "../../store/reviews"
+import { addImage, deleteImage, getOwnerReviews } from "../../store/reviews"
 import { getAllSpots } from "../../store/spots"
+import trash from '../../images/trash.svg'
 import { deleteOneReview } from "../../store/reviews"
 import './OwnerReviews.css'
 
 export const OwnerReviews = () => {
     const dispatch = useDispatch()
     const [spotsLoaded, setSpotsLoaded] = useState(false)
+    const [seeUrl, setSeeUrl] = useState('')
+    const [imgUrl, setImgUrl] = useState('')
+    const [deletePhoto, setDeletePhoto] = useState('')
+
     const currentUser = useSelector(state => state.session.user)
-    
+
     const reviewData = useSelector(state => state.reviews.user)
     const reviewArr = Object.values(reviewData.ReviewData)
 
@@ -25,11 +30,24 @@ export const OwnerReviews = () => {
     }, [dispatch])
 
     const deleteReview = async (reviewId) => {
-        // console.log("IM BEING DELETED")
         await dispatch(deleteOneReview(reviewId))
     }
+    async function deleteImg(id) {
+        await dispatch(deleteImage(id))
+        dispatch(getOwnerReviews())
+    }
+
+    async function addReviewImg(e) {
+        e.preventDefault()
+        const img = {
+            url: imgUrl
+        }
+        console.log(img, seeUrl)
+        await dispatch(addImage(+seeUrl, img))
+        dispatch(getOwnerReviews())
+    }
     if (!currentUser) {
-        return <Redirect to='/'/>
+        return <Redirect to='/' />
     }
 
     let ownerReviews;
@@ -48,8 +66,14 @@ export const OwnerReviews = () => {
                     </p>
                     <span className='review-span'>
                         <NavLink to={`/${review.id}/edit-review`}><div className='review-button-wrapper'><button className='manage-reviews-button'>Edit Review</button></div></NavLink>
+                        <div className='review-button-wrapper'><button className='manage-reviews-button' onClick={() => setSeeUrl(review.id)} >Add Review Photo</button></div>
                         <div className='review-button-wrapper'><button className='manage-reviews-button' onClick={() => deleteReview(review.id)} >Delete Review</button></div>
                     </span>
+                    {seeUrl === review.id && <form onSubmit={addReviewImg}>
+                        <label>Add Review Image Url</label>
+                        <input type='text' placeholder='https://...' value={imgUrl} onChange={e => setImgUrl(e.target.value)} />
+                        <button className='manage-reviews-button' type='submit'>Add Photo</button>
+                    </form>}
 
                     <div>
                         {review.ReviewImages.length > 0 && <p className='review-photos-header'>My Photos for This Spot:</p>}
@@ -57,7 +81,10 @@ export const OwnerReviews = () => {
                             {review.ReviewImages.map(obj => {
                                 return (
                                     <div>
-                                        <img className='review-img' alt={review.review} src={obj.url} />
+                                        <img className='review-img' alt={review.review} src={obj.url}
+                                            
+                                        />
+                                        {<img onClick={() => deleteImg(obj.id)} alt='trash' src={trash} className='delete-photo' />}
                                     </div>
                                 )
                             })}
@@ -74,13 +101,13 @@ export const OwnerReviews = () => {
             </div>
             <div className='review-wrapper-wrapper'>
 
-            {reviewArr.length ? (<div className='review-wrapper'>
-                {ownerReviews}
-            </div>) :
-            <div id='owner-no-reviews'>
-                You haven't written any reviews yet.
-            </div> 
-            }
+                {reviewArr.length ? (<div className='review-wrapper'>
+                    {ownerReviews}
+                </div>) :
+                    <div id='owner-no-reviews'>
+                        You haven't written any reviews yet.
+                    </div>
+                }
             </div>
         </>
     )
